e850fa413bbf48f39ca7161cdb0421a74656d610
Zaka Elab <zakaelab@openmailbox.org>
2016-06-30 17:57:24 +0200

    Get rid of Thing.java
    1 - Get rid of Things and let only the ants (Ant.java.
    2 - Reformat code
    3 - Add .gitignore

--- AntSim.java
+++ AntSim.java
@@ -4,130 +4,97 @@ import sim.util.*;
 
 public class AntSim extends SimState
 {
-  public SparseGrid2D world;
-  public final static boolean DEBUG = false;
+    public SparseGrid2D world;
+    public final static boolean DEBUG = false;
 
-  private int _fGridWith = 200;
-  private int _fGridHeight = 200;
-  private int _fNumAnts = 100;
-  private int _fMemorySize = 50;
-  private double _fConstantPickUp = 0.1;
-  private double _fConstantPutDown = 0.3;
-  private int _fNumThings = 1500;
-  private int _fNumThingTypes = 1;
-  private int _fAntSteps = 1;
+    private int _fGridWith = 200;
+    private int _fGridHeight = 200;
+    private int _fNumAnts = 100;
+    private int _fMemorySize = 50;
+    private double _fConstantPickUp = 0.1;
+    private double _fConstantPutDown = 0.3;
+    private int _fAntSteps = 1;
 
-  public int getNumAnts()
-  {
-    return _fNumAnts;
-  }
-
-  public void setNumAnts(int na)
-  {
-    _fNumAnts = na;
-  }
-
-  public int getMemorySize()
-  {
-    return _fMemorySize;
-  }
-
-  public void setMemorySize(int ms)
-  {
-    _fMemorySize = ms;
-  }
-
-  public double getConstantPickUp()
-  {
-    return _fConstantPickUp;
-  }
-
-  public void setConstantPickUp(double cpu)
-  {
-    _fConstantPickUp = cpu;
-  }
-
-  public double getConstantPutDown()
-  {
-    return _fConstantPutDown;
-  }
-
-  public void setConstantPutDown(double cpd)
-  {
-    _fConstantPutDown = cpd;
-  }
-
-  public int getNumThings()
-  {
-    return _fNumThings;
-  }
-
-  public void setNumThings(int nt)
-  {
-    _fNumThings = nt;
-  }
-
-  public void setNumThingTypes(int ntt)
-  {
-    if (_fNumThingTypes > 0 || _fNumThingTypes < Thing.MAX_TYPES)
-      _fNumThingTypes = ntt;
-  }
-
-  public int getNumThingTypes()
-  {
-    return _fNumThingTypes;
-  }
-
-  public int getAntSteps()
-  {
-    return _fAntSteps;
-  }
-
-  public void setAntSteps(int as)
-  {
-    _fAntSteps = as;
-  }
-
-
-  public AntSim(long seed)
-  {
-    super(seed);
-  }
-
-  public void start()
-  {
-    super.start();
-    world = new SparseGrid2D(_fGridWith, _fGridHeight);
-
-    Object object;
-    for(int type=0; type<2; type++)
+    public int getNumAnts()
     {
-      int count = (type==0?_fNumAnts:_fNumThings);
-      for(int i=0;i<count;i++)
-      {
-	if (type == 0)
-	{
-	  object = new Ant();
-	  schedule.scheduleRepeating((Steppable)object);
-	}
-	else
-	  object = new Thing(random.nextInt(_fNumThingTypes));
-
-	int x = random.nextInt(_fGridWith);
-	int y = random.nextInt(_fGridHeight);
-	while(world.getObjectsAtLocation(x,y) != null)
-	{
-	  x = random.nextInt(_fGridWith);
-	  y = random.nextInt(_fGridHeight);
-	}
-	world.setObjectLocation(object,x,y);
-      }
+        return _fNumAnts;
     }
-  }
 
-  public static void main(String[] args)
-  {
-    doLoop(AntSim.class, args);
-    System.exit(0);
-  }    
+    public void setNumAnts(int na)
+    {
+        _fNumAnts = na;
+    }
+
+    public int getMemorySize()
+    {
+        return _fMemorySize;
+    }
+
+    public void setMemorySize(int ms)
+    {
+        _fMemorySize = ms;
+    }
+
+    public double getConstantPickUp()
+    {
+        return _fConstantPickUp;
+    }
+
+    public void setConstantPickUp(double cpu)
+    {
+        _fConstantPickUp = cpu;
+    }
+
+    public double getConstantPutDown()
+    {
+        return _fConstantPutDown;
+    }
+
+    public void setConstantPutDown(double cpd)
+    {
+        _fConstantPutDown = cpd;
+    }
+
+    public int getAntSteps()
+    {
+        return _fAntSteps;
+    }
+
+    public void setAntSteps(int as)
+    {
+        _fAntSteps = as;
+    }
+
+    public AntSim(long seed)
+    {
+        super(seed);
+    }
+
+    public void start()
+    {
+        super.start();
+        world = new SparseGrid2D(_fGridWith, _fGridHeight);
+
+        Object object;
+        for(int i = 0; i < _fNumAnts; i++) {
+            object = new Ant();
+            schedule.scheduleRepeating((Steppable)object);
+
+            int x = random.nextInt(_fGridWith);
+            int y = random.nextInt(_fGridHeight);
+            while(world.getObjectsAtLocation(x,y) != null)
+                {
+                    x = random.nextInt(_fGridWith);
+                    y = random.nextInt(_fGridHeight);
+                }
+            world.setObjectLocation(object,x,y);
+
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        doLoop(AntSim.class, args);
+        System.exit(0);
+    }    
 }
--- AntSimWithUI.java
+++ AntSimWithUI.java
@@ -12,90 +12,75 @@ import javax.swing.JFrame;
 
 public class AntSimWithUI extends GUIState
 {
-  public Display2D display;
-  public JFrame displayFrame;
+    public Display2D display;
+    public JFrame displayFrame;
 
-  SparseGridPortrayal2D worldPortrayal = new SparseGridPortrayal2D();
+    SparseGridPortrayal2D worldPortrayal = new SparseGridPortrayal2D();
 
-  public AntSimWithUI() 
-  { 
-    super(new AntSim(System.currentTimeMillis())); 
-  }
+    public AntSimWithUI() 
+    { 
+        super(new AntSim(System.currentTimeMillis())); 
+    }
 
-  public AntSimWithUI(SimState state) 
-  { 
-    super(state); 
-  }
+    public AntSimWithUI(SimState state) 
+    { 
+        super(state); 
+    }
 
-  public Object getSimulationInspectedObject() 
-  { 
-    return state; 
-  }
+    public Object getSimulationInspectedObject() 
+    { 
+        return state; 
+    }
 
-  public void start()
-  {
-    super.start();
-    setupPortrayals();
-  }
+    public void start()
+    {
+        super.start();
+        setupPortrayals();
+    }
 
-  public void load(SimState state)
-  {
-    super.load(state);
-    setupPortrayals();
-  }
+    public void load(SimState state)
+    {
+        super.load(state);
+        setupPortrayals();
+    }
 
-  public void setupPortrayals()
-  {
-    final SimpleColorMap map = new SimpleColorMap(new Color[] {
-						    Color.cyan,
-						    Color.yellow,
-						    Color.magenta,
-						    Color.pink,
-						    Color.orange},
-						  5.0,Thing.MAX_TYPES,
-						  Color.blue,
-						  Color.red);
-    // tell the portrayals what to
-    // portray and how to portray them
-    worldPortrayal.setField(((AntSim)state).world);
-    worldPortrayal.setPortrayalForClass(Ant.class, 
-					 new sim.portrayal.simple.OvalPortrayal2D(Color.green));
+    public void setupPortrayals()
+    {
+        final SimpleColorMap map = new SimpleColorMap(new Color[] {
+                Color.cyan,
+                Color.yellow,
+                Color.magenta,
+                Color.pink,
+                Color.orange});
+        // tell the portrayals what to
+        // portray and how to portray them
+        worldPortrayal.setField(((AntSim)state).world);
+        worldPortrayal.setPortrayalForClass(Ant.class, 
+                                            new sim.portrayal.simple.OvalPortrayal2D(Color.green));
 
-    worldPortrayal.setPortrayalForClass(Thing.class,
-	 new RectanglePortrayal2D()
-	 {
-	   public void draw(Object object, Graphics2D graphics,
-			  DrawInfo2D info)
-	   {
-	     paint = map.getColor(((Thing)object).getType());
-	     super.draw(object,graphics,info);
-	   }
-	 }
-      );
+        // reschedule the displayer
+        display.reset();
+        // redraw the display
+        display.repaint();
+    }
 
-    // reschedule the displayer
-    display.reset();
-    // redraw the display
-    display.repaint();
-  }
+    public void init(Controller c)
+    {
+        super.init(c);
 
-  public void init(Controller c)
-  {
-    super.init(c);
+        display = new Display2D(600,600,this,1);
+        displayFrame = display.createFrame();
+        c.registerFrame(displayFrame);
+        displayFrame.setVisible(true);
 
-    display = new Display2D(600,600,this,1);
-    displayFrame = display.createFrame();
-    c.registerFrame(displayFrame);
-    displayFrame.setVisible(true);
+        display.setBackdrop(Color.white);
 
-    display.setBackdrop(Color.white);
+        // attach the portrayals
+        display.attach(worldPortrayal,"World");
+    }
 
-    // attach the portrayals
-    display.attach(worldPortrayal,"World");
-  }
-
-  public static void main(String[] args)
-  {
-    new AntSimWithUI().createController();
-  }
+    public static void main(String[] args)
+    {
+        new AntSimWithUI().createController();
+    }
 }
